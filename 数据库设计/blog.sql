11a5842c0d011d39af21c817b49e6618411b05e8
shoppingzh <1049262057@qq.com>
2019-12-31 14:17:45 +0800

    DDL SQL更新

--- 数据库设计/blog.sql
+++ 数据库设计/blog.sql
@@ -11,7 +11,35 @@ create table t_user(
     age tinyint,
     deleted tinyint not null default 0,
     gmt_create datetime,
-    gmt_modify datetime
+    gmt_modify datetime,
+	primary key(id)
 );
 
 insert into t_user (id, username, password, nickname) values ('0', 'zxp', '123', '管理员');
+
+create table t_article(
+	id varchar(256),
+	user_id varchar(256),
+	title varchar(1024) not null default '',
+	summary varchar(1024) not null default '',
+	draft tinyint not null default 1,
+	deleted tinyint not null default 0,
+	gmt_create datetime,
+	gmt_modify datetime,
+	primary key(id)
+);
+
+create table t_article_content(
+	article_id varchar(256) not null,
+	content text not null,
+	primary key(article_id)
+);
+
+create table t_tag(
+	id varchar(256),
+	name varchar(256) not null,
+	deleted tinyint not null default 0,
+	gmt_create datetime,
+	gmt_modify datetime,
+	primary key(id)
+);
